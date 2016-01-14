b228b13f76093e357f34004ce34415b811b5854e
lushan2866 <lushan2866@gmail.compwd>
2016-01-14 21:09:50 +0800

    complie之后是List结构
    utf-8 的转码

--- py2.py
+++ py2.py
@@ -3,10 +3,9 @@ import re
 import urllib2
 res= urllib2.urlopen('http://www.heibanke.com/lesson/crawler_ex00/')
 f= res.read().decode('utf-8')
-b= re.compile('b')
+b= re.compile(u'你')
 s = re.findall(b,f)
-
-print s
+print s[0].encode('utf-8')
 
 
 
