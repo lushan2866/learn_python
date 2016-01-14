a56d1ddaec05434e3d27fdd26687c5b914450ac3
lushan2866 <lushan2866@gmail.compwd>
2016-01-14 22:29:28 +0800

    complie之后是List结构
    utf-8 的转码

--- py2.py
+++ py2.py
@@ -1,11 +1,15 @@
 # -*- coding:utf-8 -*-  
 import re 
 import urllib2
-res= urllib2.urlopen('http://www.heibanke.com/lesson/crawler_ex00/')
-f= res.read().decode('utf-8')
-b= re.compile(u'你')
-s = re.findall(b,f)
-print s[0].encode('utf-8')
+
+url = 'http://www.heibanke.com/lesson/crawler_ex00/'
+def read_html():
+    res= urllib2.urlopen(url)
+    f= res.read().decode('utf-8')
+    b= re.compile(u'你需要在网址后输入数字\d{5}')
+    s = re.findall(b,f)
+    print s[0].encode('utf-8')
 
 
+read_html()
 
